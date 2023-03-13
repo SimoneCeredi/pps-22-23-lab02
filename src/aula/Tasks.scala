3268df14c5a0990e9b22e19d35d25b715af58c7b
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-13 11:24:25 +0100

    Completed task 2ab 2ac

--- src/aula/Tasks.scala
+++ src/aula/Tasks.scala
@@ -5,9 +5,34 @@ object Tasks extends App:
     case i if i >= 0 => "positive"
     case _ => "negative"
 
+
   def positive(x: Int): String = x match
     case i if i >= 0 => "positive"
     case _ => "negative"
 
-  println(positive(5))
   println(funcPositive(-5))
+  println(positive(5))
+
+
+  val funcNeg: (String => Boolean) => String => Boolean = f => s => !f(s)
+
+  def neg(f: String => Boolean): String => Boolean = s => !f(s)
+
+  val empty: String => Boolean = _ == ""
+  val funcNotEmpty = funcNeg(empty)
+  println(funcNotEmpty("foo"))
+  println(funcNotEmpty(""))
+  println(funcNotEmpty("foo") && !funcNotEmpty(""))
+
+  val notEmpty = neg(empty)
+  println(notEmpty("foo"))
+  println(notEmpty(""))
+  println(notEmpty("foo") && !notEmpty(""))
+
+  def genericNeg[X](f: X => Boolean): (X => Boolean) = x => !f(x)
+
+  val genericPositive: Int => Boolean = _ >= 0
+  val genericNotPositive = genericNeg(genericPositive)
+  println(genericNotPositive(1))
+  println(genericNotPositive(-1))
+  println(genericNotPositive(1) && !genericNotPositive(-1))
